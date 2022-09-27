830e9cb5e6c576a75cfc0e30054740e0f792ee57
Ashwin <ashwin.m@bfhl.in>
2022-09-27 21:08:06 +0530

    feat: added venue bookings api with date

--- src/core/modules/venue/venueController.js
+++ src/core/modules/venue/venueController.js
@@ -8,4 +8,6 @@ venueController.get("/:venueId", venueService.getSingleVenue);
 
 venueController.post("/", venueService.createVenue);
 
+venueController.post("/getBookings", venueService.getVenueBookingsByDate)
+
 module.exports = venueController;
--- src/core/modules/venue/venueService.js
+++ src/core/modules/venue/venueService.js
@@ -1,39 +1,65 @@
 const Venue = require("./models/venueModel");
 const successResponseMapper = require("../../common/utils/successResponseMapper");
 const failureResponseMapper = require("../../common/utils/failureResponseMapper");
+const VenueStats = require("../bookings/models/venueStats");
+const moment = require("moment");
 
 
-module.exports.getAllVenues = async function (req, res) { 
-    const venues = await Venue.find({}, {
-        name: 1,
-        imageUrls: 1,
-    });
-    console.log(`No. of venues fetched: ${venues.length}`);
-    if (venues && venues.length) { 
-        return res.status(200).send(successResponseMapper(venues));
+module.exports.getAllVenues = async function (req, res) {
+  const venues = await Venue.find(
+    {},
+    {
+      name: 1,
+      imageUrls: 1,
     }
-    return res.status(404).send(failureResponseMapper("No venues available"));
-}
+  );
+  console.log(`No. of venues fetched: ${venues.length}`);
+  if (venues && venues.length) {
+    return res.status(200).send(successResponseMapper(venues));
+  }
+  return res.status(404).send(failureResponseMapper("No venues available"));
+};
 
 module.exports.getSingleVenue = async function (req, res) {
-    try {
-        const venue = await Venue.findById(req.params.venueId);
-        if (venue) {
-            return res.status(200).send(successResponseMapper(venue));
-        }
-    } catch (err) {
-        return res.status(404).send(failureResponseMapper("No venues available with this Id"))
-    };
-}
+  try {
+    const venue = await Venue.findById(req.params.venueId);
+    if (venue) {
+      return res.status(200).send(successResponseMapper(venue));
+    }
+  } catch (err) {
+    return res
+      .status(404)
+      .send(failureResponseMapper("No venues available with this Id"));
+  }
+};
 
-module.exports.createVenue = async function (req, res) { 
-    try {
-        const data = req.body;
-        const venue = await Venue.create(data);
-        return res.status(200).send(successResponseMapper(venue));
-    }
-    catch (err) { 
-        console.log(err);
-        res.status(400).send(failureResponseMapper(err.message));
-    }
-}
+module.exports.createVenue = async function (req, res) {
+  try {
+    const data = req.body;
+    const venue = await Venue.create(data);
+    return res.status(200).send(successResponseMapper(venue));
+  } catch (err) {
+    console.log(err);
+    res.status(400).send(failureResponseMapper(err.message));
+  }
+};
+
+module.exports.getVenueBookingsByDate = async function (req, res) {
+  try {
+    const { venueId, date } = req.body;
+    const stats = await VenueStats.findOne(
+      {
+        venue: venueId,
+        bookingDate: moment(new Date(date)).format("LL"),
+      },
+      { __v: 0 }
+    )
+      .populate("slots.user", { __v: 0 })
+      .populate("slots.booking", { __v: 0 })
+      .lean();
+
+    return res.status(200).send(successResponseMapper(stats));
+  } catch (err) {
+    res.status(400).send(failureResponseMapper(err.message));
+  }
+};
