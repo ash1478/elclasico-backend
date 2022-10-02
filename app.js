90558e77d35707a989b54d05ac7306b0e97f66e2
Ashwin <ashwin.m@bfhl.in>
2022-10-02 22:54:48 +0530

    fix: Added website url under cors

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
-const express = require('express');
-const cors = require('cors');
-const baseRouter = require('./src/core/common/routerModuleMapper');
-const initializeMongo = require('./src/repository/mongodb/mongooseClient');
+const express = require("express");
+const cors = require("cors");
+const baseRouter = require("./src/core/common/routerModuleMapper");
+const initializeMongo = require("./src/repository/mongodb/mongooseClient");
 const app = express();
-const dotenv = require('dotenv').config();
+const dotenv = require("dotenv").config();
 
-app.use(cors({
-    origin: "*",
-    credentials: true
-}))
+app.use(
+  cors({
+    origin: [process.env.PORTAL_URL],
+  })
+);
 
-app.use(express.json())
+app.use(express.json());
 
 app.use("/", baseRouter);
 
 app.listen(process.env.PORT || 3010, () => {
-    initializeMongo();
-    console.log("App is running on port 3010");
-})
+  initializeMongo();
+  console.log("App is running on port 3010");
+});
