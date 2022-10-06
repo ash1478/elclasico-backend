a5ba663119e4c24b87ac16a8b7d317d66106a5bc
Ashwin <ashwin.m@bfhl.in>
2022-10-06 20:43:39 +0530

    Added additional cors handling

--- app.js
+++ app.js
@@ -24,6 +24,15 @@ app.options(
 
 app.use(express.json());
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use("/", baseRouter);
 
 app.listen(process.env.PORT || 3010, () => {
